c640f543e2cb2cfd26d0c41d62dd40c495e754b2
williamsandst <williamsandstrom99@hotmail.com>
2020-08-01 18:14:28 +0200

    Worked on Neural net implementation

--- src/Model/NeuralNetImp.h
+++ src/Model/NeuralNetImp.h
@@ -4,15 +4,18 @@
 const int input_size = 13 * 64 + 4 + 8; //Bitboard pieces, castling and en passant
 const int output_size = 64 * 64 + 32 + 1; //From and to square plus promotions and evaluation neuron
 const int linear_size = 13 * 64 + 4 + 8;
+const int conv_size = 256;
 
 struct NeuralNetImpl : torch::nn::Module {
 
-    torch::nn::Linear input_weights, output_weights, linear1, linear2, linear3;
-    //nn::Conv2d conv1, conv2;
+     torch::nn::Linear input_weights, output_weights, linear1, linear2, linear3;
+    //torch::nn::ConvTranspose2d conv1;
 
     NeuralNetImpl();
 
     torch::Tensor forward(torch::Tensor& input);
 };
+
+//input_weights, output_weights,
 
 TORCH_MODULE_IMPL(NeuralNet, NeuralNetImpl);
